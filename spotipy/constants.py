cfedcf25b735b16d44b7ba096f812c5bfbc5ed0d
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-02-09 14:38:56 +0200

    Add const variables

---
+++ spotipy/constants.py
@@ -0,0 +1,12 @@
+class Path:
+    SONGS_FILES_PATH = "songs\\"
+
+
+class SongFileKeys:
+    TRACK_KEY_NAME = "track"
+    ALBUM_KEY_NAME = "album"
+    ARTISTS_LIST_KEY_NAME = "artists"
+
+    TRACK_ID_NAME = ALBUM_ID_NAME = ARTIST_ID_NAME = "id"
+    TRACK_NAME_NAME = ALBUM_NAME_NAME = ARTIST_NAME_NAME = "name"
+    TRACK_POPULARITY_NAME = "popularity"
