bb100ccf3ca5f11c0e2897e1ea923280b87b512c
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-02-10 16:24:19 +0200

    Other searches are working too

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -28,16 +28,67 @@ def search():
     return render_template("search.html", options=search_options)
 
 
-@app.route('/Artists_names', methods=['GET'])
+@app.route('/search/Artists_names', methods=['GET'])
 def search_artists_names():
     try:
         results = spotipy.curr_user.search_any_request(SearchForUser(spotipy.curr_user).get_artists_names)
-        return "\n".join(results)
+        return "<br>".join(results)
     except ArtistIdNotFound or AlbumIdNotFound as e:
         return "Invalid input: " + str(e)
     except Exception as e:
         return str(e)
-    
+
+
+@app.route('/search/Albums_of_artist', methods=['GET'])
+def search_albums_of_artist():
+    return render_template("search_parameter.html", param_name="artist id")
+
+
+@app.route('/search/Albums_of_artist', methods=['POST'])
+def show_albums_of_artist():
+    try:
+        param = request.form['param']
+        results = spotipy.curr_user.search_any_request(SearchForUser(spotipy.curr_user).albums_of_artist, param)
+        return "<br>".join(results)
+    except ArtistIdNotFound or AlbumIdNotFound as e:
+        return "Invalid input: " + str(e)
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/search/Top_10_tracks_of_artist', methods=['GET'])
+def search_top_10_tracks_of_artist():
+    return render_template("search_parameter.html", param_name="artist id")
+
+
+@app.route('/search/Top_10_tracks_of_artist', methods=['POST'])
+def show_top_10_tracks_of_artist():
+    try:
+        param = request.form['param']
+        results = spotipy.curr_user.search_any_request(SearchForUser(spotipy.curr_user).sorted_ten_tracks_by_popularity_of_artist, param)
+        return "<br>".join(results)
+    except ArtistIdNotFound or AlbumIdNotFound as e:
+        return "Invalid input: " + str(e)
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/search/Tracks_of_album', methods=['GET'])
+def search_tracks_of_album():
+    return render_template("search_parameter.html", param_name="album id")
+
+
+@app.route('/search/Tracks_of_album', methods=['POST'])
+def search_tracks_of_album():
+    try:
+        param = request.form['param']
+        results = spotipy.curr_user.search_any_request(SearchForUser(spotipy.curr_user).tracks_of_album, param)
+        return "<br>".join(results)
+    except ArtistIdNotFound or AlbumIdNotFound as e:
+        return "Invalid input: " + str(e)
+    except Exception as e:
+        return str(e)
+
 
 def run():
     app.run(host='0.0.0.0', port=2345)
