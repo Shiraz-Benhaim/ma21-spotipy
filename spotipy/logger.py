0ecad6b22f2794555aa6b788b30d0e88a4f638dc
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-02-10 10:55:52 +0200

    Remove console appender

--- spotipy/logger.py
+++ spotipy/logger.py
@@ -5,19 +5,12 @@ def create_logger():
     log = logging.getLogger('spotify_logger')
     log.setLevel(logging.DEBUG)
 
-    # debug and the higher log levels will be printed to the console
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
     # warning and the higher log levels will be printed to a file
     fh = logging.FileHandler('log.log')
-    fh.setLevel(logging.WARNING)
+    fh.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
 
     log.addHandler(fh)
-    log.addHandler(ch)
-
     return log
