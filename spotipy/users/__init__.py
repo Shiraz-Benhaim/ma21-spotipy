5753b152b7e2d90f9b0649b10b8e19e65bb36556
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-02-10 08:29:32 +0200

    Put the creation of a user as a note

--- spotipy/users/__init__.py
+++ spotipy/users/__init__.py
@@ -20,12 +20,12 @@ def login(username, password):
             spotipy.log.info(f"Login attempt failed because file '{username}' is in bad format")
             raise e
     else:
-        """ DEBUG"""
+        """ DEBUG
         user_type = 0  # TODO: check if username is in artists
         user_file = {UserFileKeys.PASSWORD_KEY_NAME: password,
                      UserFileKeys.USER_TYPE_KEY_NAME: 0,
                      UserFileKeys.PLAYLIST_LIST_KEY_NAME: []}
         utils.Utils.write_to_file(f"{Path.USERS_DIR}\\{username}{Suffix.JSON}", json.dumps(user_file))
-
+        """
         spotipy.log.info(f"Login attempt failed because the user '{username}' does not exist")
         raise UserDoesNotExist(f"User '{username}' does not exist")
