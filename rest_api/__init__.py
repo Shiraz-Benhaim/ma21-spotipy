5bfc3a2be9fa0f1911d25cf0773d5f7b17752ef1
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-02-10 16:11:37 +0200

    Search for artists names is working

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 import spotipy
+from spotipy import SearchForUser, ArtistIdNotFound, AlbumIdNotFound
 
 app = Flask(__name__)
 
@@ -16,10 +17,27 @@ def login():
     password = request.form['password']
     try:
         spotipy.curr_user = spotipy.login(username, password)
-        return f"hello {username}"
+        return redirect(url_for('search'))
     except Exception as e:
         return "User does not exist"
 
+
+@app.route('/search', methods=['GET'])
+def search():
+    search_options = ["Artists_names", "Albums_of_artist", "Top_10_tracks_of_artist", "Tracks_of_album"]
+    return render_template("search.html", options=search_options)
+
+
+@app.route('/Artists_names', methods=['GET'])
+def search_artists_names():
+    try:
+        results = spotipy.curr_user.search_any_request(SearchForUser(spotipy.curr_user).get_artists_names)
+        return "\n".join(results)
+    except ArtistIdNotFound or AlbumIdNotFound as e:
+        return "Invalid input: " + str(e)
+    except Exception as e:
+        return str(e)
+    
 
 def run():
     app.run(host='0.0.0.0', port=2345)
